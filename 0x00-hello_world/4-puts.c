286e2d0e97b756f4fb75adfb1e23c0a868894f53
hossiy16 <yhosaina@gmail.com>
2022-06-04 12:59:34 -0700

    task 4 done

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,19 @@
 #include <stdio.h>
+
 /**
-*description : program to print some text
-*/
+
+ * Description: main - Prints out a string to stdout.
+
+ * Return: 0 if success.
+
+ */
+
 int main(void)
+
 {
+
 puts("\"Programming is like building a multilingual puzzle");
+
 return (0);
+
 }
