2784094f917957f80c038dbe1b354e0970098912
hossiy16 <yhosaina@gmail.com>
2022-06-05 08:07:03 -0700

    task 4 done

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,21 +1,27 @@
-#include<stdio.h>                                                                                                                       
+#include <stdio.h>
 
-/**                                                                                                                                     
 
-* main - Entry point                                                                                                                    
 
-*                                                                                                                                       
+/**
 
-* Return: Always 0 (Success)                                                                                                            
+* *main - Entry point
 
-*/                                                                                                                                      
+*
 
-int main(void)                                                                                                                          
+* *
 
-{                                                                                                                                       
+*
 
-puts("\"Programming is like building a multilingual puzzle");                                                                           
+* * Return: Always 0 (Success)
 
-return (0);                                                                                                                             
+*/
+
+int main(void)
+
+{
+
+puts("\"Programming is like building a multilingual puzzle");
+
+return (0);
 
 }
