84ad7657fc91217e3aa19d406096d1c3173c4b9d
hossiy16 <yhosaina@gmail.com>
2022-06-06 06:02:09 -0700

    task 1 done

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+#include <stdlib.h>
+
+#include <time.h>
+
+
+
+int main(void)
+
+{
+
+      int x;
+
+
+
+      srand(time(0));
+
+      x = rand();
+
+
+
+      printf("%d \n", x);
+
+
+
+      /* Our own code will go here */
+
+      return 0;
+
+}
+
